947ea07a65979a2cf9232288977909a36a850d99
Vanillariis <vrmo22@student.aau.dk>
2025-04-15 13:25:12 +0200

    PlayerMovment Scripts (gamle input)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Asign Variables 
+    public float speed;
+
+    public float jumpForce;
+
+    private float move;
+
+    public Rigidbody2D playerBody;
+
+    public Vector2 castBoxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        playerBody.linearVelocity = new Vector2(speed * move, playerBody.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            playerBody.AddForce(new Vector2(playerBody.linearVelocity.x, jumpForce));
+        }
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, castBoxSize, 0, -transform.up, castDistance, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position-transform.up * castDistance, castBoxSize);
+    }
+}
